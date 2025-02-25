fd2e5c3c6ed0c4e19506f8831fef8d49615b5af0
affan818 <ansariaffan818@gmail.com>
2025-02-25 18:00:29 +0530

    update header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,16 +6,13 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Theatre Booking
+          Show Booking
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="https://stayatbasera.com/">
               Back to Browns
-            </Nav.Link>
-            <Nav.Link as={Link} to="/bookings">
-              Book Now
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
